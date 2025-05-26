3720f6871a889c6d6df15ec8e08f4fb707473a08
Shraddha Chauhan <142193557+Sc22117@users.noreply.github.com>
2025-05-27 01:13:03 +0530

    Update sendScheduler.cjs

--- functions/sendScheduler.cjs
+++ functions/sendScheduler.cjs
@@ -2,7 +2,8 @@
 const admin = require("firebase-admin");
 const Brevo = require("sib-api-v3-sdk");
 
-const serviceAccount = require("./serviceAccountKey.json");
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
+
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
